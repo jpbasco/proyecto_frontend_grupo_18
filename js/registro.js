094f32e014f4100cfd54e833f2377371a7b38f8a
jpbasco <148489603+jpbasco@users.noreply.github.com>
2024-05-23 17:14:07 -0300

    modificacion formulario

--- js/registro.js
+++ js/registro.js
@@ -1,29 +1,52 @@
-document.getElementById('formulario_registro').addEventListener('submit', function(event){
-    
+document.getElementById('formularioRegistro').addEventListener('submit', function(event) {
     let valid = true;
 
-    const nombre = document.getElementById('nombre_ususario').value;
-    if (nombre ===''){
-        alert("Ingrese su nombre");
+    // Validar Nombre
+    const nombre = document.getElementById('nombre').value;
+    if (nombre === '') {
+        alert("Ingrese un nombre valido.");
         valid = false;
     }
-    const apellido = document.getElementById('apellido_usuario').value;
-    if (apellido === ''){
-        alert("Ingrese su apellido");
-        valid = false;
-    }
+
+    // Validar Edad
     const edad = document.getElementById('edad').value;
-    if (edad === '' || isNaN(edad) || edad <= 0){
-        alert("Ingrese una edad valida");
+    if (edad === '' || isNaN(edad) || edad <= 0) {
+        alert("Ingrese una edad valida.");
         valid = false;
     }
+
+    // Validar Email
     const email = document.getElementById('email').value;
-    const caracteres = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-    if (!caracteres.test(email)){
-        alert("Ingrese un e-mail valido");
+    const emailCaracteres = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailCaracteres.test(email)) {
+        alert("Ingrese un e-mail valido.");
         valid = false;
     }
-    if (!valid){
+
+    // Validar Teléfono
+    const telefono = document.getElementById('telefono').value;
+    const telefonoCaracteres = /^[0-9]{10}$/;
+    if (!telefonoCaracteres.test(telefono)) {
+        alert("El numero de telefono debe contener 10 numeros.");
+        valid = false;
+    }
+
+    // Validar Términos y Condiciones
+    const terminos = document.getElementById('terminos').checked;
+    if (!terminos) {
+        alert("No acepto los terminos y condiciones.");
+        valid = false;
+    }
+
+    // Validar Comentarios
+    const comentarios = document.getElementById('comentarios').value;
+    if (comentarios.length > 200) {
+        alert("Los comentarios no pueden exceder los 200 caracteres.");
+        valid = false;
+    }
+
+    // Evitar el envío del formulario si no es válido
+    if (!valid) {
         event.preventDefault();
     }
 });
